679711f93405d46dc69c8beaac34af0b2619b43b
Nikita Tretyakov <tretyakov.me@gmail.com>
2023-03-28 12:22:13 +0200

    Add support for FHIR R5 specification

--- spec/download_spec_bundle.sh
+++ spec/download_spec_bundle.sh
@@ -1,10 +1,31 @@
 set -e
 
-curl --output definitions.json.zip https://hl7.org/fhir/definitions.json.zip
-shasum --algorithm 256 --check bundle.checksumfile
-unzip definitions.json.zip "definitions.json/profiles-types.json" "definitions.json/profiles-resources.json"
-mv -v definitions.json/profiles-types.json fhir.types.json
-mv -v definitions.json/profiles-resources.json fhir.resources.json
+function download_r4() {
+	curl --output definitions.json.zip https://hl7.org/fhir/R4B/definitions.json.zip
+	shasum --algorithm 256 --check r4.bundle.checksumfile
+	unzip definitions.json.zip "definitions.json/profiles-types.json" "definitions.json/profiles-resources.json"
+	mv -v definitions.json/profiles-types.json fhir.types.json
+	mv -v definitions.json/profiles-resources.json fhir.resources.json
 
-rm -rf definitions.json/
-rm definitions.json.zip
+	rm -rf definitions.json/
+	rm definitions.json.zip
+}
+
+function download_r5() {
+	curl --output definitions.json.zip https://hl7.org/fhir/R5/definitions.json.zip
+	shasum --algorithm 256 --check r5.bundle.checksumfile
+	unzip definitions.json.zip "profiles-types.json" "profiles-resources.json"
+	mv -v profiles-types.json fhir.types.json
+	mv -v profiles-resources.json fhir.resources.json
+
+	rm definitions.json.zip
+}
+
+case "$1" in
+	"r5")
+		download_r5
+	;;
+	*)
+		download_r4
+	;;
+esac
